c9520a3f55d9e6d3487d2da9b235ef3c274a16d6
youssef ismail <youssefismail09@gmail.com>
2026-05-10 19:13:55 +0100

    Backend Cold Chain

--- models/mqttModel.js
+++ models/mqttModel.js
@@ -3,20 +3,24 @@ const mongoose = require("mongoose");
 const mqttSchema = new mongoose.Schema({
   device_id: {
     type: String,
-    required: true,
+    required: [true, "Device ID is required !!!"],
   },
+
   temperature: {
-    type: String,
-    default: "-200",
+    type: Number,
+    required: [true, "Temperature is required !!!"],
   },
+
   humidity: {
-    type: String,
-    default: "-200",
+    type: Number,
+    required: [true, "Humidity is required !!!"],
   },
+
   timestamp: {
     type: Date,
-    default: Date.now(),
+    default: Date.now, 
   },
+
   company_id: {
     type: String,
     default: "Unknown",
